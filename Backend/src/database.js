4777d4c8dcb76a7f4093fec094b23309edca7bc8
ggilmay <ggilmay@eafit.edu.co>
2019-08-17 11:35:40 -0500

    m

--- Backend/src/database.js
+++ Backend/src/database.js
@@ -1,13 +1,6 @@
 const mongoose = require('mongoose');
 
-
-mongoose.connect('mongodb://localhost/comments').then(() => {
-console.log("Connected to Database");
-}).catch((err) => {
-    console.log("Not Connected to Database ERROR! ", err);
-});
-
-/*console.log(process.env.MONGODB_URI)
+console.log(process.env.MONGODB_URI)
 const URI = process.env.MONGODB_URI;
 
 mongoose.connect(URI, {
@@ -20,5 +13,5 @@ const connection = mongoose.connection;
 
 connection.once('open', () => {
     console.log('DB is connected');
-})*/
+})
 
